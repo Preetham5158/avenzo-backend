1ae5162514566737e296638d62e5e8fa54166f46
Preetham M <preetham@uxltechnologies.net>
2026-05-04 16:23:24 +0530

    added menu UI

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(express.static("public"));
 
 const BASE_URL = process.env.BASE_URL || "http://localhost:5000";
 
